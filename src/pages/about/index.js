9380989555c1971a49518d19e0917a55d26a2717
ktno <kittinan.ktt@gmail.com>
2020-08-12 11:40:09 +0700

    feat: add first section of About

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,39 +1,78 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { aboutAction } from '../../actions'
-import { Typography } from 'antd'
+import { Row, Col, Typography, Avatar, Divider, Card } from 'antd'
+import cover from '../../assets/images/cover.png'
+import profile from '../../assets/images/profile.jpg'
+import javascript from '../../assets/images/javascript.png'
+import react from '../../assets/images/react.png'
+import './style.css'
 
 const About = () => {
   const { Title, Text, Paragraph } = Typography
+  const roles = ['developer', 'learner', 'dreamer', 'gamer', 'cat lover']
+
+  useEffect(() => {
+    console.log(roles[0])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [roles[0]])
 
   return (
-    <div className='content'>
-      <Typography>
-        <Title className='headline'>I'm Kittinan,</Title>
-        <Paragraph className='subheading'>a developer.</Paragraph>
-        <Title className='heading'>About</Title>
-      </Typography>
-      <Typography>
-        <Title className='headline'>I'm Kittinan,</Title>
-        <Paragraph className='subheading'>a developer.</Paragraph>
-        <Title className='heading'>About</Title>
-      </Typography>
-      <Typography>
-        <Title className='headline'>I'm Kittinan,</Title>
-        <Paragraph className='subheading'>a developer.</Paragraph>
-        <Title className='heading'>About</Title>
-      </Typography>
-      <Typography>
-        <Title className='headline'>I'm Kittinan,</Title>
-        <Paragraph className='subheading'>a developer.</Paragraph>
-        <Title className='heading'>About</Title>
-      </Typography>
-      <Typography>
-        <Title className='headline'>I'm Kittinan,</Title>
-        <Paragraph className='subheading'>a developer.</Paragraph>
-        <Title className='heading'>About</Title>
-      </Typography>
-    </div>
+    <Col className='content'>
+      {/* first section - welcome */}
+      <div
+        style={{
+          backgroundImage: `url(${cover})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          height: 'calc(100vh - 400px)'
+        }}
+      >
+        <Row
+          className='section'
+          style={{ marginTop: 0 }}
+          align='middle'
+          justify='center'
+        >
+          <Col span={24}>
+            <Title
+              className='headline'
+              style={{
+                marginBottom: 0,
+                filter: 'drop-shadow(0 0 0.2rem black)'
+              }}
+            >
+              I'm Kittinan.
+            </Title>
+            <Row align='middle' justify='center'>
+              <Paragraph
+                className='subheading'
+                style={{
+                  marginBottom: 0,
+                  filter: 'drop-shadow(0 0 0.1rem black)'
+                }}
+              >
+                And I'm a
+              </Paragraph>
+              <div className='role subheading'>
+                <div className='role_container'>
+                  <ul className='role_container_list'>
+                    {roles.map(role => (
+                      <li
+                        className='role_container_list_item'
+                        style={{ filter: 'drop-shadow(0 0 0.1rem black)' }}
+                      >
+                        {role}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </Row>
+          </Col>
+        </Row>
+      </div>
   )
 }
 
