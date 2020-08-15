be506e08b22fa3f983418ba35a5ec601fe595cd7
ktno <kittinan.ktt@gmail.com>
2020-08-15 19:35:54 +0700

    refactor: import all images from index file

---
+++ src/assets/images/index.js
@@ -0,0 +1,47 @@
+import alibabacloud from './alibabacloud.png'
+import antdesign from './antdesign.png'
+import bootstrap from './bootstrap.png'
+import cover from './cover.png'
+import css3 from './css3.png'
+import docker from './docker.png'
+import firebase from './firebase.png'
+import github from './github.png'
+import googlecloudplatform from './googlecloudplatform.png'
+import html5 from './html5.png'
+import javascript from './javascript.png'
+import jsx from './jsx.png'
+import kmitl from './kmitl.png'
+import mockflow from './mockflow.jpg'
+import mongodb from './mongodb.png'
+import nginx from './nginx.png'
+import nodejs from './nodejs.png'
+import profile from './profile.jpg'
+import react from './react.png'
+import sourcetree from './sourcetree.png'
+import tangerine from './tangerine.png'
+import tuaton from './tuaton.png'
+
+export {
+  alibabacloud,
+  antdesign,
+  bootstrap,
+  cover,
+  css3,
+  docker,
+  firebase,
+  github,
+  googlecloudplatform,
+  html5,
+  javascript,
+  jsx,
+  kmitl,
+  mockflow,
+  mongodb,
+  nginx,
+  nodejs,
+  profile,
+  react,
+  sourcetree,
+  tangerine,
+  tuaton
+}
